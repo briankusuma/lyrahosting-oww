993a636190c6892c48e2c5f0c1bcf4c18c75f5c3
briankusuma <kusuma.brian@gmail.com>
2025-02-04 14:39:05 +0700

    build: tab-content=section-discover

--- js/script.js
+++ js/script.js
@@ -36,3 +36,22 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+
+// logic tab discover
+
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".tab-btn");
+    const contents = document.querySelectorAll(".tab-content");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+
+            buttons.forEach(btn => btn.classList.remove("active"));
+            contents.forEach(content => content.classList.remove("active"));
+
+            button.classList.add("active");
+            document.getElementById(button.dataset.tab).classList.add("active");
+        });
+    });
+});
